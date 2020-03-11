f01b42855fa70b8cb3907773d4737525fb1ab9b1
unknown <sowetnikov2010@yandex.ru>
2020-03-11 18:39:06 +0300

    init: cat-count and carbon-date

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,17 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(num) {
+  let result;
+  if(typeof(num) ==typeof("qwe")){
+    if(parseInt(num)){
+      let x = parseFloat(num);
+      let k = 0.693/HALF_LIFE_PERIOD;
+      result = Math.ceil(Math.log(MODERN_ACTIVITY/x)/k);
+      if(result <0||!result) return false;
+      return result;
+    }
+    return false;
+  }
+  return false;
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,10 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+
+module.exports = function countCats(arr) {
+  let counter = 0;
+  arr = [].concat(...arr);
+  //console.log(arr);
+  for (let i =0;i<arr.length;i++){
+    if(arr[i] == "^^") counter++;
+  }
+  return counter;
 };
